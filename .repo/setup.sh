4bf6f39739601c79267a02328e39f2de6c3d19fe
Nicholas Paun <np@icebergsystems.ca>
2014-10-19 17:01:56 -0700

    Added helpful repository tools.

---
+++ .repo/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#setup.sh: A script for setting up the development environment
+#(C) Copyright 2014 Nicholas Paun
+
+GITDIR=`git rev-parse --git-dir`
+echo "Enabling post-commit hook"
+echo "#!/bin/sh" >> $GITDIR/hooks/post-commit
+echo >> $GITDIR/hooks/post-commit
+
+echo "\$GIT_DIR/../.repo/runtags &" >> $GITDIR/hooks/post-commit
+
+chmod u+x $GITDIR/hooks/post-commit
+
