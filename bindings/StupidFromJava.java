04cf2c0c360160e3099831501eea4a5e603ea671
Nicholas Paun <np@icebergsystems.ca>
2014-10-21 09:42:10 -0700

    Reorganized app/ directory, as well.

---
+++ bindings/StupidFromJava.java
@@ -0,0 +1,27 @@
+package ca.nicholaspaun.cafeteria;
+import jp.ac.kobe_u.cs.prolog.lang.*;
+
+public class StupidFromJava {
+    public static String getCapital(String country) {
+	PrologControl prolog = new PrologControl();
+	Predicate capital = new PRED_capital_2();
+	Term t_country = SymbolTerm.makeSymbol(country);
+	Term t_capital = new VariableTerm();
+	Term[] args = { t_country, t_capital };
+	//prolog.setPredicate(capital, args);
+/*
+	for (boolean r = prolog.call(); r; r = prolog.redo()) {
+	    System.out.println(t_capital.toString());
+	}
+*/
+
+	if (prolog.execute(capital,args))
+	 return(t_capital.toString());
+	else
+	 return("no.");
+    }
+
+    public static void main(String[] args) {
+	System.out.println(getCapital(args[0]));
+    }
+}
