6116b94ffa176a6017c4412d456bc2cc8b5c2f62
Nicholas Paun <np@icebergsystems.ca>
2014-12-18 08:27:56 -0800

    Added asset file installer, running in UI thread.

--- app/MainActivity.java
+++ app/MainActivity.java
@@ -1,26 +1,35 @@
 package ca.nicholaspaun.chemlogic.app1;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 
 public class MainActivity extends ActionBarActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		install();
 		setContentView(R.layout.activity_main);
 		if (savedInstanceState == null) {
 			getSupportFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
+		
+
 	}
 
 	@Override
@@ -58,4 +67,80 @@ public class MainActivity extends ActionBarActivity {
 			return rootView;
 		}
 	}
+	
+	public void install () {   
+      
+
+            copyAssetFolder(getAssets(), "files", 
+                    "/data/data/ca.nicholaspaun.chemlogic.app1/files");
+ 
+    }
+
+    private static boolean copyAssetFolder(AssetManager assetManager,
+            String fromAssetPath, String toPath) {
+        try {
+            String[] files = assetManager.list(fromAssetPath);
+            new File(toPath).mkdirs();
+            boolean res = true;
+            for (String file : files)
+            {
+          
+            	String[] contents = assetManager.list(fromAssetPath + "/" + file);
+            	
+            	
+            	
+            	
+                if (contents.length == 0)
+                {
+                    res &= copyAsset(assetManager, 
+                            fromAssetPath + "/" + file,
+
+                            toPath + "/" + file);
+                    
+                    Log.w("chemlogic",toPath);
+                    Log.w("chemlogic",file);
+                                      
+                    	Runtime.getRuntime().exec("chmod 700 " + toPath + "/" + file);
+                }
+                else 
+                    res &= copyAssetFolder(assetManager, 
+                            fromAssetPath + "/" + file,
+                            toPath + "/" + file);
+            }
+            return res;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private static boolean copyAsset(AssetManager assetManager,
+            String fromAssetPath, String toPath) {
+        InputStream in = null;
+        OutputStream out = null;
+        try {
+          in = assetManager.open(fromAssetPath);
+          new File(toPath).createNewFile();
+          out = new FileOutputStream(toPath);
+          copyFile(in, out);
+          in.close();
+          in = null;
+          out.flush();
+          out.close();
+          out = null;
+          return true;
+        } catch(Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private static void copyFile(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[2 * 1024 * 1024];
+        int read;
+        while((read = in.read(buffer)) != -1){ 
+          out.write(buffer, 0, read);
+        }
+    }
+
 }
